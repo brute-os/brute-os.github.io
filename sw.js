8f84847560a2d577017332670bf09192b0ab72ca
sys41x4 <cybersec.arijitbhowmick@gmail.com>
2023-06-21 02:31:14 +0530

    Updated index.html with additional key addition commands

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230621.0207';
+const cacheName = 'chirpy-20230621.0227';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
